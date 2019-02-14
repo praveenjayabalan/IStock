146d98c23128d4729038d79cd75b986526554ab4
praveenjayabalan <pravin2007.cbe@gmail.com>
2019-02-14 19:40:21 +0530

    ItemModel

---
+++ IStockAPI/IStockAPI/ViewModels/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IStockAPI.ViewModels
+{
+    public class Item
+    {
+        [Key]
+        public Int32 Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter ItemCode")]
+        [EmailAddress]
+        [StringLength(10)]
+        public string ItemCode { get; set; }
+
+        [Required(ErrorMessage = "Please Enter ItemName")]
+        [StringLength(100)]
+        public string ItemName { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+    }
+
+}
